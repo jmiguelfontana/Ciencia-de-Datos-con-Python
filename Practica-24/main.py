87737176f0aae1525d17f42d095ae29d51ae56f2
Juan Miguel Fontana <jmiguelfontana@gmail.com>
2025-07-11 18:36:58 +0200

    Práctica 24

---
+++ Practica-24/main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+def incrementar(x):
+    if x > 40:
+        return x * 1.25
+    else:
+        return x
+   
+def main():
+    pedidos = pd.DataFrame([
+        {"Cliente": "Comercial Suministros", "Cantidad": 45, "Precio": 100.0},
+        {"Cliente": "Distribuciones Alimentarias", "Cantidad": 5, "Precio": 50.0},
+        {"Cliente": "Servicios Informáticos", "Cantidad": 20, "Precio": 200.0},
+        {"Cliente": "Comercial Suministros", "Cantidad": 55, "Precio": 150.0},
+        {"Cliente": "Servicios Informáticos", "Cantidad": 10, "Precio": 100.0}
+    ])
+    print(pedidos)              
+    pedidos['Cantidad 3'] = pedidos['Cantidad'].apply(incrementar)
+    print(pedidos)
+
+main()
